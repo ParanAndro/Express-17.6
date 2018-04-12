a6096de643e10fa51bdd4c7d7dad482e84d67a6a
Radek Izdebski <radek1izdebski@gmail.com>
2018-04-12 16:31:23 +0200

    Add package.json

---
+++ Express-17.6/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use('/store', function(req, res, next){
+	console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+	next();
+});
+
+app.get('/', function (req, res) {
+	res.send('Hello world!');
+});
+
+app.get('/store', function(req, res) {
+	res.send('To jest sklep!');
+})
+
+app.listen(3002);
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});

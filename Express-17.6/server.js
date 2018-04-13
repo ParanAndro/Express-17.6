0a197f501a6472283280ef689cba23e6f5ef1797
Radek Izdebski <radek1izdebski@gmail.com>
2018-04-13 11:18:36 +0200

    Add files to check

--- Express-17.6/server.js
+++ Express-17.6/server.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
 app.use('/store', function(req, res, next){
-	console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+	console.log('Jestem pośrednikiem przy żądaniu do /store');
 	next();
 });
 
 app.get('/', function (req, res) {
-	res.send('Hello world!');
+	res.send('Hello World');
 });
 
-app.get('/store', function(req, res) {
-	res.send('To jest sklep!');
-})
+app.get('/store', function (req, res) {
+	res.send('To jest sklep');
+});
 
-app.listen(3002);
+app.get('/first-template', function(req, res) {
+	res.render('first-template');
+});
+
+app.get('/auth/google', function(req, res) {
+	res.render('google-log');
+});
+
+app.get('/logout', function(req, res) {
+	res.render('google-user');
+});
+
+app.listen(3003);
 app.use(function (req, res, next) {
 	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
